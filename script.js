1bd235567df5ab9d7fbd886c5b6cc1aa5ebeb749
pradorodolfoj-beep <pradorodolfoj@gmail.com>
2026-07-19 23:33:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const defaultProducts = [
         name: "Ensalada de Frutas Suprema",
         desc: "Deliciosa mezcla de fresas, cambur, kiwi y melón, bañada con leche condensada y queso rallado.",
         price: 4.50,
-        image: "imagenes/imágenes (1).webp",
+        image: "imágenes/image.jpg",
         addons: ["Helado Extra+1.50", "Chocolate+1.00"],
         removals: ["Sin Leche Condensada", "Sin Queso"]
     },
